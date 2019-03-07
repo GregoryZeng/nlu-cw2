ff3760eadb5d0dfc66d628284f4234286bf8058c
zengsh <gregzeng@qq.com>
2019-03-07 00:27:26 +0000

    modified tensors to accommodate gpu

--- seq2seq/models/lstm.py
+++ seq2seq/models/lstm.py
@@ -314,9 +314,14 @@ class LSTMDecoder(Seq2SeqDecoder):
         if cached_state is not None:
             tgt_hidden_states, tgt_cell_states, input_feed = cached_state
         else:
-            tgt_hidden_states = [torch.zeros(tgt_inputs.size()[0], self.hidden_size) for i in range(len(self.layers))]
+            # tgt_hidden_states = [torch.zeros(tgt_inputs.size()[0], self.hidden_size) for i in range(len(self.layers))]
+            tgt_hidden_states = [src_embeddings.new_full((tgt_inputs.size()[0], self.hidden_size),0)
+                                 for _ in range(len(self.layers))]
             # tgt_inputs: (batch_size, time_steps)
-            tgt_cell_states = [torch.zeros(tgt_inputs.size()[0], self.hidden_size) for i in range(len(self.layers))]
+            # tgt_cell_states = [torch.zeros(tgt_inputs.size()[0], self.hidden_size) for i in range(len(self.layers))]
+            tgt_cell_states = [src_embeddings.new_full((tgt_inputs.size()[0], self.hidden_size),fill_value=0)
+                               for _ in range(len(self.layers))]
+            print(tgt_hidden_states[0].dtype, tgt_cell_states[0].dtype)
             input_feed = tgt_embeddings.data.new(batch_size, self.hidden_size).zero_()
             # tgt_embedding: (time_steps, batch_size, num_features)
         '''___QUESTION-1-DESCRIBE-D-END___'''
