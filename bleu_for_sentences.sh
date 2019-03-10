992069bb51d98d029fa04dee8d263d69fb182529
zengsh <gregzeng@qq.com>
2019-03-10 22:01:33 +0000

    add bleu comparisons for different translations

---
+++ bleu_for_sentences.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+rm modified_merge.txt
+
+counter=0
+reference=1
+baseline=1
+lexical=1
+
+while IFS= read -r line; do
+    if [[ "$counter" -eq 0 ]] ;then
+        counter=1
+        echo "$line" >> modified_merge.txt
+    elif [[ "$counter" -eq 1 ]]; then
+        counter=2
+        reference=${line}
+        echo "$line" >> modified_merge.txt
+    elif [[ "$counter" -eq 2 ]]; then
+        counter=3
+        baseline=${line}
+        echo "$line" >> modified_merge.txt
+    elif [[ "$counter" -eq 3 ]]; then
+        counter=4
+        lexical=${line}
+        echo "$line" >> modified_merge.txt
+    elif [[ "$counter" -eq 4 ]]; then
+        counter=0
+        perl multi-bleu.perl -lc <(printf "%s" "$reference") < <(printf "%s" "$baseline") >> modified_merge.txt
+        perl multi-bleu.perl -lc <(printf "%s" "$reference") < <(printf "%s" "$lexical")  >> modified_merge.txt
+        printf "\n" >> modified_merge.txt
+    fi
+#    echo 1
+done < merge.txt
